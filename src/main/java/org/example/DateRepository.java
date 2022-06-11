1f85ad0866bad83620dd4b3fdb86b823946f8ba9
luxlorys <bossbyrcev221@gmail.com>
2022-06-11 13:37:22 +0300

    optimized code

--- src/main/java/org/example/DateRepository.java
+++ src/main/java/org/example/DateRepository.java
@@ -4,40 +4,35 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DateRepository  implements IDateSorter{
 
-    private final static List<Month> patternWithoutR = Arrays.asList(Month.AUGUST, Month.JULY, Month.JUNE, Month.MAY);
-    private final static List<Month> patternWithR = Arrays.asList(Month.APRIL, Month.DECEMBER, Month.FEBRUARY,
-            Month.JANUARY, Month.MARCH, Month.NOVEMBER,
-            Month.OCTOBER, Month.SEPTEMBER);
     public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+
+        List<Month> patternWithoutR = Arrays.asList(Month.AUGUST, Month.JULY, Month.JUNE, Month.MAY);
+        List<Month> patternWithR = Arrays.asList(Month.APRIL, Month.DECEMBER, Month.FEBRUARY,
+                Month.JANUARY, Month.MARCH, Month.NOVEMBER,
+                Month.OCTOBER, Month.SEPTEMBER);
 
         // adding to a new collection and revers sorting months without R in the name
         Collection<LocalDate> listOfMonthWithoutR = unsortedDates
                 .stream().
-                filter(month -> patternWithoutR.contains(month.getMonth())).toList();
-
-        Collection<LocalDate> sortedWithoutR = listOfMonthWithoutR
-                .stream()
+                filter(month -> patternWithoutR.contains(month.getMonth()))
                 .sorted()
                 .toList();
+
+
 
         // adding to a new collection and sorting months with R in the name
         Collection<LocalDate> listOfMonthWithR = unsortedDates
                 .stream()
-                .filter(month -> patternWithR.contains(month.getMonth())).toList();
+                .filter(month -> patternWithR.contains(month.getMonth())).sorted().toList();
 
-        Collection<LocalDate> sortedDateWithR = listOfMonthWithR.stream()
-                .sorted()
-                .toList();
 
-        return Stream.of(sortedDateWithR, sortedWithoutR)
+        return Stream.of(listOfMonthWithR, listOfMonthWithoutR)
                 .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+                .toList();
     }
 }
